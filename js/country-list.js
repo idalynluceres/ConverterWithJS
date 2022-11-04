afb3bb8070e339b5a58f838da1a27ee274b46507
yayeen <idalynluceres22@gmail.com>
2022-11-04 23:35:31 +0800

    first commit

---
+++ js/country-list.js
@@ -0,0 +1,13 @@
+let country_code = 
+{
+    "EUR" : "FR",
+    "GBP" : "GB",
+    "INR" : "IN",
+    "KRW" : "KR",
+    "MAD" : "MA",
+    "MXN" : "MX",
+    "PHP" : "PH",
+    "PKR" : "PK",
+    "SAR" : "SA",
+    "USD" : "US",
+}
